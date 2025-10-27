045834b18aa5fb0b5f0b58b14076d433ff50176b
mj <nampink2300@gmail.com>
2025-10-27 19:25:26 +0900

    FDBD-35 fix[chat]: 임시 비활성화

--- src/main/java/com/mudosa/musinsa/global/config/websocket/SecurityConfig.java
+++ src/main/java/com/mudosa/musinsa/global/config/websocket/SecurityConfig.java
@@ -1,22 +1,24 @@
-package com.mudosa.musinsa.global.config.websocket;// SecurityConfig.java
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll()
-        );
-
-    return http.build();
-  }
-}
+//package com.mudosa.musinsa.global.config.websocket;// SecurityConfig.java
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration("webSocketSecurityConfig") // 👈 명시적 이름
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//  @Bean
+//  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http
+//        .csrf(csrf -> csrf.disable())
+//        .authorizeHttpRequests(auth -> auth
+//            .anyRequest().permitAll()
+//        );
+//
+//    return http.build();
+//  }
+//}
