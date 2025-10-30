910ae08071262c43105ac0c0d7f8b7096fea5dc1
mj <nampink2300@gmail.com>
2025-10-30 13:11:26 +0900

    FDBD-35 feat[chat]: 브랜드 관리자인지 판단 로직 추가

---
+++ src/main/java/com/mudosa/musinsa/brand/domain/repository/BrandMemberRepository.java
@@ -0,0 +1,14 @@
+package com.mudosa.musinsa.brand.domain.repository;
+
+import com.mudosa.musinsa.brand.domain.model.BrandMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * BrandMember Repository
+ */
+@Repository
+public interface BrandMemberRepository extends JpaRepository<BrandMember, Long> {
+  
+  boolean existsByBrand_BrandIdAndUserId(Long brandId, Long userId);
+}
