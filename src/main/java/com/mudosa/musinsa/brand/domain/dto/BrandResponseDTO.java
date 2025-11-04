00e4e621df46f59b46e7adc2488d17c6d3d70468
mj <nampink2300@gmail.com>
2025-11-04 17:44:20 +0900

    FDBD-59 feat[brand]: 브랜드 DTO에 대한 swagger 설명 추가

--- src/main/java/com/mudosa/musinsa/brand/domain/dto/BrandResponseDTO.java
+++ src/main/java/com/mudosa/musinsa/brand/domain/dto/BrandResponseDTO.java
@@ -1,22 +1,29 @@
 package com.mudosa.musinsa.brand.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mudosa.musinsa.product.application.dto.ProductSearchResponse;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
 import java.util.List;
 
-@Data
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Schema(description = "브랜드 + 해당 브랜드 상품 정보 반환 DTO")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BrandResponseDTO {
+  @Schema(description = "브랜드 Id", example = "1")
   public long brandId;
+  @Schema(description = "브랜드 한글 이름", example = "무신상")
   public String nameKo;
+  @Schema(description = "브랜드 영어 이름", example = "MuSinSang")
   public String nameEn;
+  @Schema(description = "로고 이미지 url", example = "/brand/logo/c166f09e-8e75-4307-8be3-63253270a459_로고.png")
   public String logoURL;
 
+  @Schema(description = "상품 리스트[6]")
   public List<ProductSearchResponse.ProductSummary> products;
 }
