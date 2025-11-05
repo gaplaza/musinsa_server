134ccb1e7aa98f7ddb91151acf700a3900e33b39
gaplaz <upaaaaa@ewhain.net>
2025-11-05 13:56:10 +0900

    fix[settlement]: 수정

--- src/main/java/com/mudosa/musinsa/ServerApplication.java
+++ src/main/java/com/mudosa/musinsa/ServerApplication.java
@@ -4,9 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
 @EnableScheduling
 @MapperScan(
     basePackages = {
